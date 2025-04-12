797cb8bfe751af89068ed4f26bb0c303cdc9683a
Kseniia <kore-ksenia@yandex.ru>
2025-04-13 00:40:36 +0300

    удален импорт

--- src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmTests.java
@@ -7,7 +7,6 @@ import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Set;
 
