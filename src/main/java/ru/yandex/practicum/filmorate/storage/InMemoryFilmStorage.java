989441ac66f8b2b214da54f4d9bfe4cd61680216
Kseniia <kore-ksenia@yandex.ru>
2025-04-26 19:54:20 +0300

    внесены правки по комментариям

--- src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -73,14 +73,6 @@ public class InMemoryFilmStorage implements FilmStorage {
     @Override
     public void removeLike(Long filmId, Long userId) {
         Film film = getFilmById(filmId);
-        if (film == null) {
-            throw new FilmNotFoundException("Фильм с ID " + filmId + " не найден");
-        }
-
-        if (!userStorage.existsUserById(userId)) {
-            throw new UserNotFoundException("Некорректный ID пользователя");
-        }
-
         film.getLikes().remove(userId);
     }
 
